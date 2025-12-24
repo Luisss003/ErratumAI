1abbeb4b1dda073f0dc409801be639491b63eb31
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-12-24 15:19:49 -0600

    initial spring boot scaffold

---
+++ textlift-backend/src/main/java/com/luis/textlift_backend/TextLiftApplication.java
@@ -0,0 +1,13 @@
+package com.luis.textlift_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextLiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextLiftApplication.class, args);
+	}
+
+}
---
+++ textlift-backend/src/test/java/com/luis/textlift_backend/TextLiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.textlift_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextLiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
