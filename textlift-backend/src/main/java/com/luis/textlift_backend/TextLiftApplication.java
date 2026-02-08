f955f82104cdae02da4a335a0733a9f06888f4ee
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2026-02-07 23:44:02 -0600

    text comments

--- textlift-backend/src/main/java/com/luis/textlift_backend/TextLiftApplication.java
+++ textlift-backend/src/main/java/com/luis/textlift_backend/TextLiftApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TextLiftApplication {
+	//test coment
 	public static void main(String[] args) {
 		SpringApplication.run(TextLiftApplication.class, args);
 	}
